92aec1ad965b50e4459a8bbe29354683b62f8ffb
Arnold Martinez <arnoldmartinez@criptext.com>
2022-04-28 21:20:47 -0500

    Adding methods to handle post, get, put, delete, http request
    Cap. 17

--- src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
@@ -1,10 +1,28 @@
 package com.appsdeveloperblog.app.ws.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("users") // http://localhost:8080/users
 public class UserController {
 
+    @GetMapping
+    public String getUser() {
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
 }

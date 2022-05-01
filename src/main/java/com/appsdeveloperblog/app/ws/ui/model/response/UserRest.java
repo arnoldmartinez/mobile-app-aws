806aee01984aac8ff5b9545533051bf5abbc09fd
Arnold Martinez <arnoldmartinez@criptext.com>
2022-04-30 23:30:46 -0500

    Returning object as JSON or XML representation Cap. 23

--- src/main/java/com/appsdeveloperblog/app/ws/ui/model/response/UserRest.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/model/response/UserRest.java
@@ -1,9 +1,21 @@
 package com.appsdeveloperblog.app.ws.ui.model.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
 public class UserRest {
+
+    @JsonProperty("firstName")
     private String firstName;
+
+    @JsonProperty("lastName")
     private String lastName;
+
+    @JsonProperty("email")
     private String email;
+
+    @JsonProperty("userID")
     private String userID;
 
     public String getFirstName() {
@@ -36,5 +48,28 @@ public class UserRest {
 
     public void setUserID(String userID) {
         this.userID = userID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserRest)) return false;
+        UserRest userRest = (UserRest) o;
+        return Objects.equals(firstName, userRest.firstName) && Objects.equals(lastName, userRest.lastName) && Objects.equals(email, userRest.email) && Objects.equals(userID, userRest.userID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, userID);
+    }
+
+    @Override
+    public String toString() {
+        return "UserRest{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", userID='" + userID + '\'' +
+                '}';
     }
 }

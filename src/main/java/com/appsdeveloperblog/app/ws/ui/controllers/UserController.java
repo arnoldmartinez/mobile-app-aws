99d7d8a80dc3d3c661229c0d0cf2a3daef5caee5
Arnold Martinez <arnoldmartinez@criptext.com>
2022-05-01 10:03:28 -0500

    Reading HTTP POST request body. The @RequestBody annotation Cap. 25

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controllers/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.app.ws.ui.controllers;
 
+import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -28,9 +29,15 @@ public class UserController {
         return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
-    @PostMapping
-    public String createUser() {
-        return "create user was called";
+    @PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+        UserRest returnValue = new UserRest();
+
+        returnValue.setEmail(userDetails.getEmail());
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
     @PutMapping
